64125411a9361442720876d7e7ed71989f8cdb39
sashaxxd <webdizain@bk.ru>
2017-12-25 19:44:06 +0300

    Работа с input range практика

--- PRAKTIKA/rabota s range/script.js
+++ PRAKTIKA/rabota s range/script.js
@@ -1,6 +1,17 @@
-var name = 'vaniya'; // Строковые string  есть с плавающей точкой булевы и т.д. как в php
-var user = 'vniya', age = 2;
-var message = 'Hello';
-var message = '2Hello';
-alert(age + ' Годика');
-alert(message);
+function fun1() {//Функция выведет значение в параграфе
+   var r1 = document.getElementById('r1');
+   var p = document.getElementById('one');
+   p.innerHTML = r1.value;
+}
+
+function fun2() {//Функция выведет значение в инпуте
+    var r1 = document.getElementById('r1');
+    var in1 = document.getElementById('in1');
+    in1.value = r1.value;
+}
+
+function fun3() {//Функция выведет значение в инпуте
+    var r1 = document.getElementById('r1');
+    var line = document.getElementById('line');
+    line.style.width = r1.value + 'px';
+}
